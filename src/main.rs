b0591f501145976b7d48a7c9fc091a95cbde41b5
Caceresenzo <caceresenzo1502@gmail.com>
2024-04-01 17:09:26 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use std::net::TcpListener;
+use std::{
+    io::{Result, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle(mut stream: TcpStream) -> Result<()> {
+    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+
+    Ok(())
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                
+                match handle(stream) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("handle error: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
