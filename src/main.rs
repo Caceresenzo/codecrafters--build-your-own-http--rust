49e1324b7a3bdd81379cd731c215ae2b6b16e292
Caceresenzo <caceresenzo1502@gmail.com>
2024-04-01 18:34:02 +0200

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,150 @@
 use std::{
-    io::{BufRead, BufReader, Result, Write},
+    collections::HashMap,
+    fmt::{self, Debug},
+    io::{BufRead, BufReader, BufWriter, Result, Write},
     net::{TcpListener, TcpStream},
 };
 
-fn handle(mut stream: TcpStream) -> Result<()> {
-    let mut reader = BufReader::new(&stream);
+#[derive(Debug)]
+enum Method {
+    Get,
+    Post,
+    Unknown,
+}
+
+impl fmt::Display for Method {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(self, f)
+    }
+}
+
+#[derive(Debug)]
+enum Status {
+    Ok,
+    // Created,
+    NotFound,
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(self, f)
+    }
+}
+
+impl Status {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Status::Ok => "200 OK",
+            // Status::Created => "201 Created",
+            Status::NotFound => "404 Not Found",
+        }
+    }
+}
+
+struct Request {
+    pub method: Method,
+    pub path: String,
+    pub version: String,
+}
+
+struct Response {
+    pub status: Status,
+    pub headers: HashMap<String, String>,
+    pub body: Vec<u8>,
+}
+
+impl Response {
+    pub fn of(status: Status) -> Response {
+        Response {
+            status,
+            headers: HashMap::new(),
+            body: Vec::new(),
+        }
+    }
+}
+
+fn parse_request(reader: &mut BufReader<&TcpStream>) -> Result<Request> {
     let mut buffer = String::new();
 
     reader.read_line(&mut buffer)?;
     let parts: Vec<&str> = buffer.split(" ").collect();
-    let _method = parts[0];
-    let path = parts[1];
-    let _version = parts[2];
 
-    if path == "/" {
-        stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
-    } else {
-        stream.write("HTTP/1.1 404 OK\r\n\r\n".as_bytes())?;
+    let method = match parts[0] {
+        "GET" => Method::Get,
+        "POST" => Method::Post,
+        _ => Method::Unknown,
+    };
+
+    let path: String = parts[1].into();
+    let version: String = parts[2].into();
+
+    Ok(Request {
+        method,
+        path: path.trim_end().into(),
+        version: version.trim_end().into(),
+    })
+}
+
+fn answer(
+    writer: &mut BufWriter<&TcpStream>,
+    request: Request,
+    response: Response,
+) -> Result<String> {
+    let space = [b' '];
+    let colon = [b':', b' '];
+    let crlf = [b'\r', b'\n'];
+
+    writer.write(request.version.as_bytes())?;
+    writer.write(&space)?;
+    writer.write(response.status.as_str().as_bytes())?;
+    writer.write(&crlf)?;
+    
+    for (key, value) in response.headers.into_iter() {
+        writer.write(key.as_bytes())?;
+        writer.write(&colon)?;
+        writer.write(value.as_bytes())?;
+        writer.write(&crlf)?;
+    }
+    
+    writer.write(&crlf)?;
+    writer.write(&response.body)?;
+
+    return Ok(format!(
+        "{} {} --> {}",
+        request.method, request.path, response.status
+    ));
+}
+
+fn route(request: &Request) -> Response {
+    if request.path == "/" {
+        return Response::of(Status::Ok);
     }
 
-    Ok(())
+    if request.path.starts_with("/echo/") {
+        let echo = &request.path[6..];
+
+        let mut headers: HashMap<String, String> = HashMap::new();
+        headers.insert("Content-Type".into(), "text/plain".into());
+        headers.insert("Content-Length".into(), echo.len().to_string());
+
+        return Response {
+            status: Status::Ok,
+            headers,
+            body: echo.as_bytes().into(),
+        };
+    }
+
+    Response::of(Status::NotFound)
+}
+
+fn handle(stream: TcpStream) -> Result<String> {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    let request = parse_request(&mut reader)?;
+    let response = route(&request);
+
+    answer(&mut writer, request, response)
 }
 
 fn main() {
@@ -33,7 +158,9 @@ fn main() {
                 println!("accepted new connection");
 
                 match handle(stream) {
-                    Ok(_) => {}
+                    Ok(msg) => {
+                        println!("ok: {}", msg);
+                    }
                     Err(e) => {
                         println!("handle error: {}", e);
                     }
