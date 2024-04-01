d5af45bc91b0665614a2cb132b467d948e968fff
Caceresenzo <caceresenzo1502@gmail.com>
2024-04-01 17:35:46 +0200

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::{
-    io::{Result, Write},
+    io::{BufRead, BufReader, Result, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn handle(mut stream: TcpStream) -> Result<()> {
-    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    let mut reader = BufReader::new(&stream);
+    let mut buffer = String::new();
+
+    reader.read_line(&mut buffer)?;
+    let parts: Vec<&str> = buffer.split(" ").collect();
+    let _method = parts[0];
+    let path = parts[1];
+    let _version = parts[2];
+
+    if path == "/" {
+        stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    } else {
+        stream.write("HTTP/1.1 404 OK\r\n\r\n".as_bytes())?;
+    }
 
     Ok(())
 }
@@ -18,7 +31,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                
+
                 match handle(stream) {
                     Ok(_) => {}
                     Err(e) => {
