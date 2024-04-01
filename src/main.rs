266c2aed35ffc5e0261add47c76e2122b6c3d0a2
Caceresenzo <caceresenzo1502@gmail.com>
2024-04-01 18:50:28 +0200

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,7 @@ struct Request {
     pub method: Method,
     pub path: String,
     pub version: String,
+    pub headers: HashMap<String, String>,
 }
 
 struct Response {
@@ -54,11 +55,23 @@ struct Response {
 }
 
 impl Response {
-    pub fn of(status: Status) -> Response {
+    pub fn status(status: Status) -> Response {
         Response {
             status,
             headers: HashMap::new(),
             body: Vec::new(),
+        }
+    }
+
+    pub fn text(text: String) -> Response {
+        let mut headers: HashMap<String, String> = HashMap::new();
+        headers.insert("Content-Type".into(), "text/plain".into());
+        headers.insert("Content-Length".into(), text.len().to_string());
+
+        Response {
+            status: Status::Ok,
+            headers,
+            body: text.as_bytes().into(),
         }
     }
 }
@@ -78,10 +91,28 @@ fn parse_request(reader: &mut BufReader<&TcpStream>) -> Result<Request> {
     let path: String = parts[1].into();
     let version: String = parts[2].into();
 
+    let mut headers: HashMap<String, String> = HashMap::new();
+
+    loop {
+        buffer.clear();
+        reader.read_line(&mut buffer)?;
+        if buffer == "\r\n" {
+            break;
+        }
+
+        let index = buffer.find(':').unwrap();
+
+        let key = &buffer[..index];
+        let value = buffer[index + 1..].trim();
+
+        headers.insert(key.into(), value.into());
+    }
+
     Ok(Request {
         method,
         path: path.trim_end().into(),
         version: version.trim_end().into(),
+        headers,
     })
 }
 
@@ -98,14 +129,14 @@ fn answer(
     writer.write(&space)?;
     writer.write(response.status.as_str().as_bytes())?;
     writer.write(&crlf)?;
-    
+
     for (key, value) in response.headers.into_iter() {
         writer.write(key.as_bytes())?;
         writer.write(&colon)?;
         writer.write(value.as_bytes())?;
         writer.write(&crlf)?;
     }
-    
+
     writer.write(&crlf)?;
     writer.write(&response.body)?;
 
@@ -117,24 +148,25 @@ fn answer(
 
 fn route(request: &Request) -> Response {
     if request.path == "/" {
-        return Response::of(Status::Ok);
+        return Response::status(Status::Ok);
     }
 
     if request.path.starts_with("/echo/") {
         let echo = &request.path[6..];
 
-        let mut headers: HashMap<String, String> = HashMap::new();
-        headers.insert("Content-Type".into(), "text/plain".into());
-        headers.insert("Content-Length".into(), echo.len().to_string());
-
-        return Response {
-            status: Status::Ok,
-            headers,
-            body: echo.as_bytes().into(),
-        };
+        return Response::text(echo.into());
     }
 
-    Response::of(Status::NotFound)
+    if request.path.starts_with("/user-agent") {
+        let user_agent = match request.headers.get("User-Agent") {
+            Some(value) => value,
+            None => "",
+        };
+
+        return Response::text(user_agent.into());
+    }
+
+    Response::status(Status::NotFound)
 }
 
 fn handle(stream: TcpStream) -> Result<String> {
